c897cc0abcaa6926efe4476f7c02c3dda7021a8b
Fernando Diaz <fdiaza@gmail.com>
2012-09-22 13:23:17 -0300

    First steps of command line parsing.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,15 @@
-int main()
+#include <iostream>
+#include <string>
+#include <vector>
+#include "rom.h"
+
+int main(int argc, char* argv[])
 {
+	std::vector<std::string> commandLine(argv, argv + argc);
+	if(commandLine.size() < 2)
+	{
+		std::cerr << "rom parameter missing" << std::endl;
+		return -1;
+	}
 	return 0;
 }
