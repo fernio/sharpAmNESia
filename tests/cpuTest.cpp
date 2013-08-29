2f198ca96138daf26eefd9b2fb74a0426590e3df
Fernando Diaz <fdiaza@gmail.com>
2013-08-28 21:57:35 -0400

    Hardcoded test rom name in cpuTest, and other improvements.

--- tests/cpuTest.cpp
+++ tests/cpuTest.cpp
@@ -2,18 +2,27 @@
 #include "nescpu.hpp"
 #include "rom.hpp"
 
+#define CPU_TEST_ROM "nestest.nes"
+
 int main(int argc, char* argv[])
 {
 	if(argc < 2)
 	{
-		std::cerr << "no rom specified" << std::endl;
+		std::cerr << "no rom path specified" << std::endl;
 		return -1;
 	}
+	std::string romFilename(argv[1]);
+	romFilename += CPU_TEST_ROM;
 	NesRom rom;
-	rom.Load(argv[1]);
+	if(!rom.Load(romFilename.c_str()))
+	{
+		std::cerr << "failed loading rom: " << romFilename << std::endl;
+		return -1;
+	}
 	NESCPU cpu;
 	cpu.SetRomPtr(rom.GetDataPtr());
 	cpu.PowerUp();
+	cpu.m_pc = 0xC000;
 	cpu.Execute(100);
 	return 0;
 }
