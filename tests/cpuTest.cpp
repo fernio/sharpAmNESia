a393cee829f8b13995d5e5e43dc80fafe533a709
Fernando Diaz <fdiaza@gmail.com>
2013-09-22 19:05:02 -0300

    CPU unit test is now checking the number of cycles emulated.

--- tests/cpuTest.cpp
+++ tests/cpuTest.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* argv[])
 {
 	if(argc < 2)
 	{
-		std::cerr << "no rom path specified" << std::endl;
+		std::cerr << "no rom path specified\n";
 		return -1;
 	}
 	std::string romFilename(argv[1]);
@@ -24,6 +24,12 @@ int main(int argc, char* argv[])
 	cpu.PowerUp();
 	//execution has to be set to start at 0xC000 according to NesDev wiki
 	cpu.m_pc = 0xC000;
-	cpu.Execute(100);
+	//TODO: find exact number of cycles that test takes
+	static int cyclesToExecute = 10000;
+	if(cpu.Execute(cyclesToExecute) != cyclesToExecute)
+	{
+		std::cerr << "unexpected number of cycles executed\n";
+		return -1;
+	}
 	return 0;
 }
